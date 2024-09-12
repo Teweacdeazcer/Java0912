aa8ad7be2d8cdabdaad0bc94947a335517db3d8f
김승민 <tmdals6102@gmail.com>
2024-09-12 11:52:42 +0900

    Java ch8 실습 1~4

---
+++ src/chp08/ex8_2/MainDrink.java
@@ -0,0 +1,11 @@
+package chp08.ex8_2;
+
+public class MainDrink {
+    public static void main(String[] args) {
+        Drink d1 = new Drink();
+        Drink d2 = new Drink("콜라", 1500);
+
+        System.out.println(d1);
+        System.out.println(d2);
+    }
+}
